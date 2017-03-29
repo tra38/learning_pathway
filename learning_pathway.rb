b47d36440a18f7e83b5360c395e214d1b2cd22a3
tra38 <tra38@nau.edu>
2017-03-29 14:11:16 -0500

    Rewrite CSV loading process
    SmarterCSV store each CSV row as its own hash, but it turns out that it doesn't
    work properly when there is no default headers (such as with our CSV file).
    Thus, it may be better to replace the SmarterCSV gem with the CSV standard libary,
    and then write some code to generate the hashes with the proper metadata that I
    plan to use for the heuristics.

--- learning_pathway.rb
+++ learning_pathway.rb
@@ -1,4 +1,4 @@
-require 'smarter_csv'
+require "csv"
 
 domain_order_csv = ARGV[0]
 student_tests_csv = ARGV[1]
@@ -8,8 +8,11 @@ def validate_csv(csv)
 end
 
 if validate_csv(domain_order_csv) && validate_csv(student_tests_csv)
-  file = SmarterCSV.process(domain_order_csv)
-  p file
+  array = []
+  CSV.foreach(File.path(domain_order_csv)) do |row|
+    array << {grade: row[0], order: row[1..-1] }
+  end
+  p array
 else
   puts "Usage: learning_pathways.rb DOMAIN_ORDER_CSV_PATH STUDENT_TEST_CSV_PATH"
 end
