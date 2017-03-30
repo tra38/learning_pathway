a05eda43d1c59e04594dd3160b83d966ca2403ff
tra38 <tra38@nau.edu>
2017-03-29 23:36:17 -0500

    Add code for heuristic
    The heurisitc will take a domain_order and then determine
    the future curriculum of students.

---
+++ heuristic.rb
@@ -0,0 +1,52 @@
+require 'set'
+require 'pry'
+
+class Heuristic
+  attr_reader :domain_order, :all_subjects, :all_grades
+  def initialize(domain_order)
+    @domain_order = domain_order
+    @all_grades = domain_order.keys
+    @all_subjects  = determine_list(domain_order)
+  end
+
+  def below_threshold?(student_grade:, subject_grade:)
+    all_grades.index(student_grade) > all_grades.index(subject_grade)
+  end
+
+  def calculate(student_row)
+
+    curriculum_array = find_valid_classes(student_row)
+
+    if curriculum_array.length > 5
+      { name: student_row["Student Name"], curriculum: curriculum_array[0..4] }
+    else
+      { name: student_row["Student Name"], curriculum: curriculum_array }
+    end
+  end
+
+  private
+
+  def determine_list(domain_order)
+    all_grades.map do |grade|
+      subjects = domain_order[grade]
+      subjects.map do |subject|
+        { course_name: "#{grade}.#{subject}", grade: grade, subject_name: subject }
+      end
+    end.flatten
+  end
+
+  def find_valid_classes(student_row)
+    reject_all_invalid_classes(student_row).map do |subject_hash|
+      subject_hash[:course_name]
+    end
+  end
+
+  def reject_all_invalid_classes(student_row)
+    all_subjects.reject do |subject|
+      subject_name = subject[:subject_name]
+      subject_grade = subject[:grade]
+      below_threshold?(student_grade: student_row[subject_name], subject_grade: subject_grade)
+    end
+  end
+
+end
---
+++ spec/heuristic_spec.rb
@@ -0,0 +1,51 @@
+require "./heuristic"
+require 'csv'
+
+RSpec.describe Heuristic do
+  context "creates a heuristic based on domain order" do
+    before(:all) do
+      domain_order = {"K"=>["RF", "RL", "RI"], "1"=>["RF", "RL", "RI"], "2"=>["RF", "RI", "RL", "L"], "3"=>["RF", "RL", "RI", "L"], "4"=>["RI", "RL", "L"], "5"=>["RI", "RL", "L"], "6"=>["RI", "RL"]}
+      @heuristic = Heuristic.new(domain_order)
+    end
+
+    it "determines the list of subjects that a person can take" do
+      expect(@heuristic.all_subjects).to eq(
+        [{:course_name=>"K.RF", :grade=>"K", :subject_name=>"RF"},
+         {:course_name=>"K.RL", :grade=>"K", :subject_name=>"RL"},
+         {:course_name=>"K.RI", :grade=>"K", :subject_name=>"RI"},
+         {:course_name=>"1.RF", :grade=>"1", :subject_name=>"RF"},
+         {:course_name=>"1.RL", :grade=>"1", :subject_name=>"RL"},
+         {:course_name=>"1.RI", :grade=>"1", :subject_name=>"RI"},
+         {:course_name=>"2.RF", :grade=>"2", :subject_name=>"RF"},
+         {:course_name=>"2.RI", :grade=>"2", :subject_name=>"RI"},
+         {:course_name=>"2.RL", :grade=>"2", :subject_name=>"RL"},
+         {:course_name=>"2.L", :grade=>"2", :subject_name=>"L"},
+         {:course_name=>"3.RF", :grade=>"3", :subject_name=>"RF"},
+         {:course_name=>"3.RL", :grade=>"3", :subject_name=>"RL"},
+         {:course_name=>"3.RI", :grade=>"3", :subject_name=>"RI"},
+         {:course_name=>"3.L", :grade=>"3", :subject_name=>"L"},
+         {:course_name=>"4.RI", :grade=>"4", :subject_name=>"RI"},
+         {:course_name=>"4.RL", :grade=>"4", :subject_name=>"RL"},
+         {:course_name=>"4.L", :grade=>"4", :subject_name=>"L"},
+         {:course_name=>"5.RI", :grade=>"5", :subject_name=>"RI"},
+         {:course_name=>"5.RL", :grade=>"5", :subject_name=>"RL"},
+         {:course_name=>"5.L", :grade=>"5", :subject_name=>"L"},
+         {:course_name=>"6.RI", :grade=>"6", :subject_name=>"RI"},
+         {:course_name=>"6.RL", :grade=>"6", :subject_name=>"RL"}]
+         )
+    end
+
+    it "determines the grade order of this school" do
+      expect(@heuristic.all_grades).to eq(["K", "1", "2", "3", "4", "5", "6"])
+    end
+
+    it "generates correct ordering for Albin Stanton" do
+      albin_stats = CSV::Row.new(["Student Name", "RF", "RL", "RI", "L"], ["Albin Stanton","2","3","K","3"])
+      expect(@heuristic.calculate(albin_stats)).to eq({ name: "Albin Stanton", curriculum: ["K.RI","1.RI","2.RF","2.RI","3.RF"] })
+    end
+  end
+end
+
+# 1) Read domain_order.csv to generate a heuristic
+# 2) Use the heuristic to order students' curriculums
+# 3) Output this curriculum into a CSV.
