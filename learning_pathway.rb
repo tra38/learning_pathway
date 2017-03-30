459806a05ad5828646c2c59dce07c87bea1928fc
tra38 <tra38@nau.edu>
2017-03-29 23:37:57 -0500

    Write glue code for reading domain_order_csv and students_test_csv
    We are able to extract data from domain_order_csv, generate a Heuristic,
    and then use that Heuristic to develop personalized curriculums for
    students based on test scores located in students_test_csv.

--- learning_pathway.rb
+++ learning_pathway.rb
@@ -1,4 +1,5 @@
 require "csv"
+require_relative "heuristic"
 
 domain_order_csv = ARGV[0]
 student_tests_csv = ARGV[1]
@@ -8,11 +9,24 @@ def validate_csv(csv)
 end
 
 if validate_csv(domain_order_csv) && validate_csv(student_tests_csv)
-  array = []
+  domain_order = {}
   CSV.foreach(File.path(domain_order_csv)) do |row|
-    array << {grade: row[0], order: row[1..-1] }
+    grade = row[0]
+    order = row[1..-1]
+    domain_order[grade] = order
   end
-  p array
+
+  heuristics = Heuristic.new(domain_order)
+
+  output_array = []
+
+  CSV.foreach(File.path(student_tests_csv), headers: true) do |row|
+    student_profile = heuristics.calculate(row)
+    generated_row = [ student_profile[:name] ] + student_profile[:curriculum]
+    output_array << generated_row
+  end
+
+  binding.pry
 else
   puts "Usage: learning_pathways.rb DOMAIN_ORDER_CSV_PATH STUDENT_TEST_CSV_PATH"
 end
